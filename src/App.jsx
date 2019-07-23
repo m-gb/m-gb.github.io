0177af85fe21e3441c2b8af3e10e47696744624e
m-gb <maya.goldberg90@gmail.com>
2019-07-23 18:04:03 +0300

    Add background color

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ class App extends Component {
 
   render() {
     return (
-      <div>
+      <div className="main">
         <Introduction />
         <NavBar />
         <main className="container">
