2b5cbfc99a77c2e096a71a68174997615464950c
m-gb <maya.goldberg90@gmail.com>
2019-07-23 03:03:46 +0300

    Add contact

---
+++ src/contact/contact.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './contact.css';
+
+class Contact extends Component {
+  render() {
+    return (
+      <div id="contact">
+        <h1 className="font-weight-bold py-5 text-center"><i className="fas fa-envelope pr-3" />Contact</h1>
+      </div>
+    );
+  }
+}
+
+export default Contact;
