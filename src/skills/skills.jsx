45b60a26793fc75093c91c581fb2ef00b3634873
m-gb <maya.goldberg90@gmail.com>
2019-12-22 21:36:51 +0200

    update skills

--- src/skills/skills.jsx
+++ src/skills/skills.jsx
@@ -17,7 +17,7 @@ class Skills extends Component {
             </tr>
             <tr>
               <th className="text-right">Frontend Frameworks</th>
-              <td>React, Angular, Ext JS</td>
+              <td>Angular, Ext JS, React</td>
               <th className="text-right">Databases</th>
               <td>PostgreSQL, MySQL, MongoDB</td>
             </tr>
