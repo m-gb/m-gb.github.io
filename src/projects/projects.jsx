1dff7d87ea8dcafdc536244fb564c575876cc1b7
m-gb <maya.goldberg90@gmail.com>
2021-02-02 21:23:12 +0200

    Redesign projects component

--- src/projects/projects.jsx
+++ src/projects/projects.jsx
@@ -4,8 +4,8 @@ import './projects.css';
 class Projects extends Component {
   render() {
     return (
-      <div id="projects" data-aos="fade-up" className="py-5">
-        <h1 className="font-weight-bold py-5 text-center"><i className="fas fa-code pr-3" />Recent Projects</h1>
+      <div id="projects" data-aos="fade-up" className="mt-5 py-5">
+        <h1 className="mt-5 font-weight-bold py-5 text-center"><i className="fas fa-code pr-3" />Recent Projects</h1>
         <div className="row">
           <div className="col-lg-4">
             <div className="card">
@@ -14,7 +14,7 @@ class Projects extends Component {
                 <h5 className="card-title">Dash of Oats</h5>
                 <p className="card-text">A cooking blog, including a calculator and a search function with suggestions</p>
                 <p className="card-text">Using Angular, Node.JS and MongoDB</p>
-                <a href="https://dashofoats.mayagoldberg.net" className="btn btn-secondary">View Project</a>
+                <a href="https://dashofoats.mayagoldberg.net" className="btn btn-dark">View Project</a>
               </div>
             </div>
           </div>
@@ -25,7 +25,7 @@ class Projects extends Component {
                 <h5 className="card-title">Starry Night</h5>
                 <p className="card-text">A weather app based on current or desired location with unit conversion</p>
                 <p className="card-text">Using React and OpenWeatherMap API</p>
-                <a href="https://m-gb.github.io/starry-night/" className="btn btn-secondary">View Project</a>
+                <a href="https://m-gb.github.io/starry-night/" className="btn btn-dark">View Project</a>
               </div>
             </div>
           </div>
@@ -36,7 +36,7 @@ class Projects extends Component {
                 <h5 className="card-title">GitHub</h5>
                 <p className="card-text">My GitHub profile where you can find all my other projects</p>
                 <p className="card-text"><br /></p>
-                <a href="https://github.com/m-gb" className="btn btn-secondary">Visit my GitHub</a>
+                <a href="https://github.com/m-gb" className="btn btn-dark">Visit my GitHub</a>
               </div>
             </div>
           </div>
